27d7c4e7b63385a53bbe21311edf02f7ab9a2e62
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-04-09 15:15:24 -0500

    Basic hello world implementation

---
+++ .history/webpack.config_20220409151121.js
@@ -0,0 +1,27 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/app/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  devServer: {
+    //....//
+    static: {                               
+      directory: path.join(__dirname, './'),  
+      watch: true
+    }
+ },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+};

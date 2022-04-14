caa5b0b0fe8da0ffec520e5741b7a2c4a9e5c8c2
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-04-14 18:14:21 -0500

    Implemented functonality to add and delete messages through the redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 function createStore(reducer, initialState) {
   let state = initialState;
   let listeners = [];
@@ -38,18 +42,49 @@ const initialState = {
 };
 
 const store = createStore(reducer, initialState)
-store.subscribe(() => console.log(`STORE STATE ${JSON.stringify(store.getState())}`));
 store.dispatch({ type: 'ADD_MESSAGE', message: 'How does it look Neil?' });
 store.dispatch({ type: 'ADD_MESSAGE', message: 'Looking good' });
 store.dispatch({ type: 'ADD_MESSAGE', message: 'Nice day' });
 store.dispatch({ type: 'ADD_MESSAGE', message: 'Beautiful day' });
 store.dispatch({ type: 'DELETE_MESSAGE', index: 2 });
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>{JSON.stringify(store.getState())}</h1>
-    </div>
-  )
+class App extends React.PureComponent {
+  state = {
+    message: ''
+  }
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
+  }
+  render() {
+    const { messages } = store.getState();
+    const listView = {
+      border: '1px solid black', 
+      marginBottom: '10px'
+    };
+    const { message } = this.state;
+    return (
+      <div>
+      <h1>Message View</h1>
+      <div style={listView}>
+        {messages && <ul style={{listStyle: 'none' }}>{messages.map((message, index) => <li onClick={() => store.dispatch({ type: 'DELETE_MESSAGE', index })} style={{ margin: '10px 0' }} key={index}>{message}</li>)}</ul>}
+      </div>
+      <Box
+        component="form"
+        sx={{
+          border: '1px solid black',
+          '& > :not(style)': { m: 1, width: '25ch' }
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField value={this.state.message} onChange={(evt) => this.setState({ message: evt.target.value })} id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="contained" onClick={() => { 
+          store.dispatch({ type: 'ADD_MESSAGE', message }); 
+          this.setState({ message: '' })
+        }}>Submit</Button>
+      </Box>
+      </div>
+    )
+    }
 }
 export default App;
